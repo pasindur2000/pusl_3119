98fefebfb9358e8532303525ff2fb367bebd1fce
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-22 19:35:09 +0530

    create onboarding_screen.dart

---
+++ lib/ui/onboarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _onboardingScreenState();
+}
+
+class _onboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
