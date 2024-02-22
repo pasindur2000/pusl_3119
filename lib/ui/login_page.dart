7f85abd875d0c516ba9465d0f1e488c491f72f6a
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-23 00:09:36 +0530

    create the all 3 pages in welcome slides

---
+++ lib/ui/login_page.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:pusl_3119/constants.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+        backgroundColor: Constants.primaryColor,
+      ),
+    );
+  }
+}
--- lib/ui/onboarding_screen.dart
+++ lib/ui/onboarding_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pusl_3119/constants.dart';
+
+import 'login_page.dart';
 
 
 class OnboardingScreen extends StatefulWidget {
@@ -23,9 +26,11 @@ class _onboardingScreenState extends State<OnboardingScreen> {
           Padding(
               padding: const EdgeInsets.only(right: 20, top: 20),
               child: InkWell(
-                onTap: ()=> null, //to login screen
+                onTap: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> const Login()));//to login screen
+                },
                 child: const Text('Skip', style: TextStyle(
-                  color: Colors.grey,
+                  color: Color(0xff296e48),
                   fontSize: 16.0,
                   fontWeight: FontWeight.w400
                 ),),
@@ -34,6 +39,147 @@ class _onboardingScreenState extends State<OnboardingScreen> {
           )
         ],
       ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView(
+            onPageChanged: (int page){
+              setState(() {
+                currentIndex = page;
+              });
+            },
+            controller: _pageController,
+            children: [
+              createPage(
+                image: 'assets/images/1st.png',
+                title: Constants.titleOne,
+                description: Constants.descriptionOne,
+              ),
+              createPage(
+                image: 'assets/images/1st.png',
+                title: Constants.titleTwo,
+                description: Constants.descriptionTwo,
+              ),
+              createPage(
+                image: 'assets/images/1st.png',
+                title: Constants.titleThree,
+                description: Constants.descriptionThree,
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 80,
+            left: 180,
+
+            child: Row(
+              children: _buildIndicator(),
+            ),),
+
+          Positioned(
+              bottom: 60,
+              right: 30,
+
+              child: Container(
+                child: IconButton(
+                  onPressed: (){
+                    setState(() {
+                      if(currentIndex < 2){
+                        currentIndex++;
+                        if(currentIndex < 3){
+                          _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+                        }
+                      }else{
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> const Login()));
+                      }
+                    });
+                  },
+                  icon: const Icon(Icons.arrow_forward_ios, size: 24, color: Colors.white,)
+                ),
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Constants.primaryColor,
+                ),
+
+              ))
+
+        ],
+      ),
+    );
+  }
+
+  //Extra Widgets
+  //Create the indicator decorations widget
+  Widget _indicator(bool isActive){
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: 10.0,
+      width: isActive? 20 : 8,
+      margin: const EdgeInsets.only(right: 5.0),
+      decoration: BoxDecoration(
+        color: Constants.primaryColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+
+  //create the indicator list
+  List<Widget> _buildIndicator(){
+    List<Widget> indicators = [];
+
+    for(int i=0; i<3; i++){
+      if(currentIndex == i){
+        indicators.add(_indicator(true));
+      }else{
+        indicators.add(_indicator(false));
+      }
+    }
+    return indicators;
+  }
+
+
+}
+
+class createPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  const createPage({
+    super.key, required this.image, required this.title, required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 50, right: 50,bottom: 80),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 450, child: Image.asset(image),),
+          const SizedBox(height: 20,),
+          Text(title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Constants.primaryColor,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20,),
+          Text(description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+
+          const SizedBox(height: 20,),
+        ],
+      ),
     );
   }
 }
+
