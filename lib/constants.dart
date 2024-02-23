3d0060034d9efaf85a8ca73a1bb75fc6a5368051
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-23 12:16:41 +0530

    create a new directory named models and inside model directory create plants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
 
   //onboarding texts
   static var titleOne = "Welcome to Disector!";
