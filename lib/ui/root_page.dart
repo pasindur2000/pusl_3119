1c7cdf822ca701d47f1f5b71604a1ce63fb06961
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-23 13:16:18 +0530

    edit the root page

--- lib/ui/root_page.dart
+++ lib/ui/root_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:pusl_3119/ui/screens/diseases_page.dart';
 import 'package:pusl_3119/ui/screens/home_page.dart';
 import 'package:pusl_3119/ui/screens/profile_page.dart';
+import 'package:page_transition/page_transition.dart';
+
+import '../constants.dart';
+import 'login_page.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({Key? key}) : super(key: key);
@@ -23,19 +27,49 @@ class _RootPageState extends State<RootPage> {
   //List of the pages icons
   List<IconData> iconList = [
     Icons.home,
-    Icons.favorite,
     Icons.shopping_cart,
     Icons.person,
   ];
 
+  //List of theh pages titles
+  List<String> titleList = [
+    'Home',
+    'Diseases',
+    'Profile',
+  ];
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Root Page'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(titleList[_bottomNavIndex], style: TextStyle(
+              color: Constants.blackColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 24,
+            ),),
+            Icon(Icons.notifications, color: Constants.blackColor, size: 30.0,)
+          ],
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
       ),
+      body: IndexedStack(
+        index: _bottomNavIndex,
+        children: pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, PageTransition(child: const Login(), type: PageTransitionType.bottomToTop));
+        },
+      ),
+
+
+
+
     );
   }
 }
