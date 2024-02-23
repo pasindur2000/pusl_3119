0c4875f1ae148dc219321f4a6a294d9183317743
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-23 12:50:25 +0530

    create the lists of the pages inside the root file

--- lib/ui/root_page.dart
+++ lib/ui/root_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pusl_3119/ui/screens/diseases_page.dart';
+import 'package:pusl_3119/ui/screens/home_page.dart';
+import 'package:pusl_3119/ui/screens/profile_page.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({Key? key}) : super(key: key);
@@ -8,6 +11,25 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int _bottomNavIndex = 0;
+
+  //List of the pages
+  List<Widget> pages = const [
+    HomePage(),
+    DiseasesPage(),
+    ProfilePage(),
+  ];
+
+  //List of the pages icons
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
