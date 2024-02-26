6e6d82c744da49d79f3629c57ddab6923c8117ce
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-02-26 12:24:05 +0530

    Added code to center the 'Select Images' button at the bottom of the ScanPage. and get Image Picker library to select images from files

--- lib/ui/screens/scan_page.dart
+++ lib/ui/screens/scan_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
@@ -13,6 +15,25 @@ class _ScanPageState extends State<ScanPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Scan Page'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(), // Add any content you want above the button here
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text("Select Images"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
